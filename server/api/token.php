65705a5093cb688bf7d5c79d4fbfc2c61e265055
nioc <dev@nioc.eu>
2016-02-09 23:16:54 +0100

    Add token API
    Provide a token creation API
    Part of #5 (Allow users authentification)

---
+++ server/api/token.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Authenticate user and create a token.
+ *
+ * Provides a token required for others API call
+ *
+ * @version 1.0.0
+ *
+ * @api
+ */
+include_once $_SERVER['DOCUMENT_ROOT'].'/server/lib/Api.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/server/lib/User.php';
+$api = new Api('json', ['POST']);
+switch ($api->method) {
+    case 'POST':
+        if (!$api->checkParameterExists('login', $login) || !$api->checkParameterExists('password', $password)) {
+            $api->output(400, 'Both login and password must be provided');
+            //login or password was not provided
+            return;
+        }
+        $user = new User();
+        if (!$user->checkCredentials($login, $password)) {
+            $api->output(401, 'Invalid credentials');
+            header('WWW-Authenticate: Bearer realm="WMP"');
+            //invalid credentials
+            return;
+        }
+        $api->output(201, $api->generateToken($user->getProfile()));
+        break;
+}
