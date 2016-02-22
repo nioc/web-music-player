nioc/web-music-player

commit 459d19771b95e8f0ab011a6894f26b551e587b41

Change route base URL

display/files/wmp.js
@@ -252,12 +252,12 @@ function MenuController(LocalUser, $window) {
     menu.visible = false;
     menu.items = [];
     var existingItems = [
-        {require: 'user', label: 'Player', icon: 'fa-headphones', link: '/player'},
-        {require: 'user', label: 'Library', icon: 'fa-archive', link: '/library'},
-        {require: 'admin', label: 'Catalog', icon: 'fa-folder-open', link: '/catalog'},
-        {require: 'user', label: 'Profile', icon: 'fa-user', link: '/profile'},
-        {require: 'admin', label: 'Admin', icon: 'fa-sliders', link: '/admin'},
-        {require: 'user', label: 'Sign out', icon: 'fa-sign-out', link: '/sign-out'},
+        {require: 'user', label: 'Player', icon: 'fa-headphones', link: '#player'},
+        {require: 'user', label: 'Library', icon: 'fa-archive', link: '#library'},
+        {require: 'admin', label: 'Catalog', icon: 'fa-folder-open', link: '#catalog'},
+        {require: 'user', label: 'Profile', icon: 'fa-user', link: '#profile'},
+        {require: 'admin', label: 'Admin', icon: 'fa-sliders', link: '#admin'},
+        {require: 'user', label: 'Sign out', icon: 'fa-sign-out', link: '#sign-out'},
         {require: 'user', label: 'Find an issue ?', icon: 'fa-bug', link: 'https://github.com/nioc/web-music-player/issues/new'},
         {require: 'user', label: 'Contribute', icon: 'fa-code-fork', link: 'https://github.com/nioc/web-music-player#contributing'}
    ];
@@ -371,5 +371,4 @@ function config($routeProvider, $locationProvider) {
     .otherwise({
         redirectTo: '/player'
     });
-    $locationProvider.html5Mode(true);
 }

display/player.html
@@ -22,7 +22,7 @@
             </nav>
         </div>
         <div class="container" ng-show="!menu.visible">
-        <div class="player" ng-controller="PlayerController as player" ng-show="menu.currentPage.link === '/player'">
+        <div class="player" ng-controller="PlayerController as player" ng-show="menu.currentPage.link === '#player'">
             <div class="player-cover">
                 <img class="player-cover" src="/display/files/images/default_cover.png" alt="cover"/>
             </div>

.htaccess
@@ -1,9 +1,8 @@
 #rewriting
 RewriteEngine on
-RewriteRule ^$ /display/index.html [L]
+RewriteRule ^$ /display/player.html [L]
 RewriteRule ^sign$ /display/index.html [L]
 RewriteRule ^sign-out$ /display/sign-out.html [L]
-RewriteRule ^player$ /display/player.html [L]
 RewriteRule ^catalog$ /display/catalog.html [L]
 RewriteRule ^library$ /display/library.html [L]
 RewriteRule ^profile$ /display/profile.html [L]

display/files/sign.controller.js
@@ -22,7 +22,7 @@
             function successCallback(response) {
                 if (user.handleToken(response.data)) {
                     //redirect user to the main page
-                    $window.location = '/player';
+                    $window.location = '/';
                 } else {
                     errorCallback(response);
                 }

commit 325ac8f9039263bbe8c8e4a6dd2333dda74a83c6

Fix bug on navigation
Fix a regression caused by commit 459d19771b95e8f0ab011a6894f26b551e587b41

display/files/wmp.js
@@ -256,12 +256,12 @@ function MenuController(LocalUser, $window) {
     menu.visible = false;
     menu.items = [];
     var existingItems = [
-        {require: 'user', label: 'Player', icon: 'fa-headphones', link: '#player'},
-        {require: 'user', label: 'Library', icon: 'fa-archive', link: '#library'},
-        {require: 'admin', label: 'Catalog', icon: 'fa-folder-open', link: '#catalog'},
-        {require: 'user', label: 'Profile', icon: 'fa-user', link: '#profile'},
-        {require: 'admin', label: 'Admin', icon: 'fa-sliders', link: '#admin'},
-        {require: 'user', label: 'Sign out', icon: 'fa-sign-out', link: '#sign-out'},
+        {require: 'user', label: 'Player', icon: 'fa-headphones', link: '#/player'},
+        {require: 'user', label: 'Library', icon: 'fa-archive', link: '#/library'},
+        {require: 'admin', label: 'Catalog', icon: 'fa-folder-open', link: '#/catalog'},
+        {require: 'user', label: 'Profile', icon: 'fa-user', link: '#/profile'},
+        {require: 'admin', label: 'Admin', icon: 'fa-sliders', link: '#/admin'},
+        {require: 'user', label: 'Sign out', icon: 'fa-sign-out', link: '#/sign-out'},
         {require: 'user', label: 'Find an issue ?', icon: 'fa-bug', link: 'https://github.com/nioc/web-music-player/issues/new'},
         {require: 'user', label: 'Contribute', icon: 'fa-code-fork', link: 'https://github.com/nioc/web-music-player#contributing'}
    ];
@@ -288,7 +288,7 @@ function MenuController(LocalUser, $window) {
     };
     //highlight current page
     function isCurrentPage() {
-        return this.link === $window.location.pathname;
+        return this.link === $window.location.hash;
     }
     //store the next page and hide menu
     function setCurrentPage() {

display/player.html
@@ -22,7 +22,7 @@
             </nav>
         </div>
         <div class="container" ng-show="!menu.visible">
-        <div class="player" ng-controller="PlayerController as player" ng-show="menu.currentPage.link === '#player'">
+        <div class="player" ng-controller="PlayerController as player" ng-show="menu.currentPage.link === '#/player'">
             <div class="player-cover">
                 <img class="player-cover" src="/display/files/images/default_cover.png" alt="cover"/>
             </div>