nioc/web-music-player

commit 53cde8954c8df5b643da8fb8d73977528c4901e0

Add scope and users management
Part of #25.
Store user identifier during API request for allow checking his scope.
Add a method in API class for checking if user is granted on the required scope.
According to RFC 6749, transform token scope from array to space-delimited strings.

display/files/wmp.js
@@ -21,7 +21,9 @@ angular
 //declare sign-out controller
 .controller('SignOutController', ['LocalUser', '$window', SignOutController])
 //declare profile controller
-.controller('UserController', ['LocalUser', 'User', UserController])
+.controller('UserController', ['LocalUser', 'User', '$routeParams', UserController])
+//declare users management controller
+.controller('UsersController', ['User', UsersController])
 //declare album controller
 .controller('AlbumController', ['$routeParams', 'Album', AlbumController])
 //declare artist controller
@@ -260,6 +262,7 @@ function MenuController(LocalUser, $window) {
         {require: 'user', label: 'Library', icon: 'fa-archive', link: '#/library'},
         {require: 'admin', label: 'Catalog', icon: 'fa-folder-open', link: '#/catalog'},
         {require: 'user', label: 'Profile', icon: 'fa-user', link: '#/profile'},
+        {require: 'admin', label: 'Users management', icon: 'fa-users', link: '#/users'},
         {require: 'admin', label: 'Admin', icon: 'fa-sliders', link: '#/admin'},
         {require: 'user', label: 'Sign out', icon: 'fa-sign-out', link: '#/sign-out'},
         {require: 'user', label: 'Find an issue ?', icon: 'fa-bug', link: 'https://github.com/nioc/web-music-player/issues/new'},
@@ -321,11 +324,17 @@ function SignOutController(LocalUser, $window) {
     $window.location = '/sign';
 }
 //ProfileController function
-function UserController(LocalUser, User) {
+function UserController(LocalUser, User, $routeParams) {
     var profile = this;
     profile.result = {text: '', class: ''};
-    LocalUser.getProfile();
-    profile.user = User.get({id: LocalUser.id});
+    if ($routeParams && $routeParams.id) {
+        //get user profile from url id parameter
+        profile.user = User.get({id: $routeParams.id});
+    } else {
+        //get local user profile
+        LocalUser.getProfile();
+        profile.user = User.get({id: LocalUser.id});
+    }
     profile.submit = submit;
     function submit() {
         function successCallback(response) {
@@ -341,6 +350,11 @@ function UserController(LocalUser, User) {
         }
         profile.user.$update(successCallback, errorCallback);
     }
+}
+//UsersController function
+function UsersController(User) {
+    var usersManagement = this;
+    usersManagement.users = User.query();
 }
 //AlbumController function
 function AlbumController($routeParams, Album) {
@@ -384,6 +398,16 @@ function config($routeProvider, $locationProvider) {
         controllerAs: 'catalog'
     })
     .when('/profile', {
+        templateUrl: '/profile',
+        controller: 'UserController',
+        controllerAs: 'profile'
+    })
+    .when('/users', {
+        templateUrl: '/users',
+        controller: 'UsersController',
+        controllerAs: 'usersManagement'
+    })
+    .when('/users/:id', {
         templateUrl: '/profile',
         controller: 'UserController',
         controllerAs: 'profile'

.htaccess
@@ -1,11 +1,12 @@
 #rewriting
 RewriteEngine on
-RewriteRule ^$ /display/player.html [L]
-RewriteRule ^sign$ /display/index.html [L]
-RewriteRule ^sign-out$ /display/sign-out.html [L]
-RewriteRule ^catalog$ /display/catalog.html [L]
-RewriteRule ^library$ /display/library.html [L]
-RewriteRule ^albums$ /display/album.html [L]
-RewriteRule ^artists$ /display/artist.html [L]
-RewriteRule ^profile$ /display/profile.html [L]
-RewriteRule ^stream/([0-9]+)$ /server/track.php?track=$1 [QSA,L]
+RewriteRule ^$                   /display/player.html [L]
+RewriteRule ^sign$               /display/index.html [L]
+RewriteRule ^sign-out$           /display/sign-out.html [L]
+RewriteRule ^catalog$            /display/catalog.html [L]
+RewriteRule ^library$            /display/library.html [L]
+RewriteRule ^albums$             /display/album.html [L]
+RewriteRule ^artists$            /display/artist.html [L]
+RewriteRule ^profile$            /display/profile.html [L]
+RewriteRule ^users$              /display/users.html [L]
+RewriteRule ^stream/([0-9]+)$    /server/track.php?track=$1 [QSA,L]

display/users.html
@@ -1,0 +1,19 @@
+            <section>
+                <h2><i class="fa fa-users"></i>Users management</h2>
+                <ul class="grid" ng-cloak>
+                    <li class="grid-header">
+                        <span class="grid-cell">login</span>
+                        <span class="grid-cell">name</span>
+                        <span class="grid-cell">email</span>
+                        <span class="grid-cell">scope</span>
+                        <span class="grid-cell"></span>
+                    </li>
+                    <li class="grid-row" ng-repeat="user in usersManagement.users track by user.sub">
+                        <span class="grid-cell" ng-bind="::user.login"></span>
+                        <span class="grid-cell" ng-bind="::user.name"></span>
+                        <span class="grid-cell" ng-bind="::user.email"></span>
+                        <span class="grid-cell" ng-bind="::user.scope"></span>
+                        <span class="grid-cell"><a ng-href="/#/users/{{::user.sub}}" title="Edit user"><i class="fa fa-pencil"></i></a></span>
+                    </li>
+                </ul>
+            </section>

server/api/.htaccess
@@ -4,6 +4,7 @@ RewriteRule ^library/tracks/?$                             library.php [L]
 RewriteRule ^library/folders/?$                            folder.php [L]
 RewriteRule ^users/tokens/?$                               token.php [L]
 RewriteRule ^users/([0-9]+)/?$                             user.php?id=$1 [L]
+RewriteRule ^users/?$                                      user.php [L]
 RewriteRule ^albums/([0-9]+)/?$                            album.php?id=$1 [L]
 RewriteRule ^albums/?$                                     album.php [L]
 RewriteRule ^artists/([0-9]+)/?$                           artist.php?id=$1 [L]

server/api/user.php
@@ -15,16 +15,34 @@ include_once $_SERVER['DOCUMENT_ROOT'].'/server/lib/User.php';
 $api = new Api('json', ['GET', 'PUT']);
 switch ($api->method) {
     case 'GET':
-        //returns the user profile
         if (!$api->checkAuth()) {
             //User not authentified/authorized
             return;
         }
         if (!$api->checkParameterExists('id', $id)) {
-            $api->output(400, 'User identifier must be provided');
-            //user was not provided, return an error
+            //without 'id' parameter, users list is requested, check if current user is granted
+            if (!$api->checkScope('admin')) {
+                $api->output(403, 'Admin scope is required for listing users');
+                //current user has no admin scope, return forbidden
+                return;
+            }
+            //returns all users
+            $user = new User();
+            $rawUsers = $user->getAllUsers();
+            if ($rawUsers === false) {
+                $api->output(500, 'Error while querying');
+                //return an internal error
+                return;
+            }
+            $users = array();
+            foreach ($rawUsers as $user) {
+                array_push($users, $user->getProfile());
+            }
+            $api->output(200, $users);
+            //return users list
             return;
         }
+        //returns the requested user profile
         $user = new User($id);
         if (!$user->populate()) {
             $api->output(404, 'User not found');

server/configuration/create.sql
@@ -11,6 +11,14 @@ CREATE TABLE `wmp`.`user` (
   UNIQUE KEY `login` (`login`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT 'Users';
 
+CREATE TABLE `wmp`.`scope` (
+  `userId` smallint(6) unsigned NOT NULL AUTO_INCREMENT COMMENT 'User identifier',
+  `scope` varchar(32) CHARACTER SET utf8 DEFAULT NULL COMMENT 'Scope',
+  PRIMARY KEY (`userId`,`scope`),
+  KEY `userId` (`userId`),
+  CONSTRAINT `scope_fk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT 'Users scope';
+
 CREATE TABLE `wmp`.`artist` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Identifier',
   `name` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT 'Artist name',
@@ -76,3 +84,5 @@ CREATE TABLE `wmp`.`playlist` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT 'User\'s current playlist';
 
 INSERT INTO `wmp`.`user` (`id`, `login`, `name`, `email`, `password`, `status`) VALUES (1, 'admin', 'Admin', NULL, md5('nimda'), 1);
+INSERT INTO `wmp`.`scope` (`userId`, `scope`) VALUES (1, 'admin'), (1, 'user');
+

server/lib/Api.php
@@ -33,6 +33,10 @@ class Api
      * @var string Returned data
      */
     private $responseBody;
+    /**
+     * @var int User identifier who is requesting API
+     */
+    private $requesterId;
 
     /**
      * Initializes an API object with the given informations.
@@ -44,6 +48,7 @@ class Api
     {
         $this->outputFormat = $outputFormat;
         $this->allowedMethods = $allowedMethods;
+        $this->requesterId = null;
         //check call method
         if (isset($_SERVER['REQUEST_METHOD'])) {
             $this->method = $_SERVER['REQUEST_METHOD'];
@@ -146,8 +151,29 @@ class Api
             //Token do not includes user profile
             return false;
         }
+        $this->requesterId = (int) $token->payload->sub;
         //Token is valid, returns the user identifier
-        return $token->payload->sub;
+        return $this->requesterId;
+    }
+
+    /**
+     * Check the if the user have a correct authentication and authorization.
+     *
+     * @param string $requiredScope Required user scope for processing the API
+     *
+     * @return bool Return if user has the required scope or not
+     */
+    public function checkScope($requiredScope)
+    {
+        if (is_int($this->requesterId)) {
+            include_once $_SERVER['DOCUMENT_ROOT'].'/server/lib/User.php';
+            $requester = new User($this->requesterId);
+            $scope = $requester->getScope();
+            //return if required scope is found in user scope
+            return in_array($requiredScope, $scope);
+        }
+        //requester is not identified, return false
+        return false;
     }
 
     /**

server/lib/User.php
@@ -30,9 +30,13 @@ class User
      */
     public $password;
     /**
-     * @var bool USer status
+     * @var bool User status
      */
     public $status;
+    /**
+     * @var array User scope
+     */
+    public $scope;
 
     /**
      * Initializes a User object with his identifier.
@@ -174,6 +178,26 @@ class User
     }
 
     /**
+     * Return user scope.
+     *
+     * @return array User scope (list of granted scopes)
+     */
+    public function getScope()
+    {
+        global $connection;
+        include_once $_SERVER['DOCUMENT_ROOT'].'/server/lib/Connection.php';
+        $query = $connection->prepare('SELECT `scope` FROM `scope` WHERE `userId`=:userId;');
+        $query->bindValue(':userId', $this->id, PDO::PARAM_INT);
+        if ($query->execute()) {
+            $this->scope = $query->fetchAll(PDO::FETCH_COLUMN);
+            //return scope
+            return $this->scope;
+        }
+        //indicate there was an error during scope querying
+        return false;
+    }
+
+    /**
      * Return public profile.
      *
      * @return object A public version of user profile
@@ -185,9 +209,27 @@ class User
         $user->login = $this->login;
         $user->name = $this->name;
         $user->email = $this->email;
-        //@TODO get user roles and populate scope array
-        $user->scope = ['user'];
+        //get user scope as a list of space-delimited strings (see https://tools.ietf.org/html/rfc6749#section-3.3)
+        $user->scope = implode(' ', $this->getScope());
         //returns the user public profile
         return $user;
+    }
+
+    /**
+     * Return all users.
+     *
+     * @return array All users
+     */
+    public function getAllUsers()
+    {
+        global $connection;
+        include_once $_SERVER['DOCUMENT_ROOT'].'/server/lib/Connection.php';
+        $query = $connection->prepare('SELECT * FROM `user`;');
+        if ($query->execute() && $query->rowCount() > 0) {
+            //return array of users
+            return $query->fetchAll(PDO::FETCH_CLASS, 'User');
+        }
+        //indicate there is a problem during querying
+        return false;
     }
 }

commit 517d93d4d70332876a70bd844db16264e41800bf

Fix parsing scope
Fix a regression caused by commit 53cde8954c8df5b643da8fb8d73977528c4901e0

display/files/wmp.js
@@ -278,8 +278,9 @@ function MenuController(LocalUser, $window) {
         return false;
     }
     //add links according to user scope
+    var scope = user.scope.split(" ");
     angular.forEach(existingItems, function(item) {
-        if (user.scope.indexOf(item.require) !== -1) {
+        if (scope.indexOf(item.require) !== -1) {
             item.isCurrentPage = isCurrentPage;
             item.setCurrentPage = setCurrentPage;
             menu.items.push(item);